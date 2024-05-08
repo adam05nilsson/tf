5cde9ca4d136818e075a612090698d627af57a66
adam05nilsson <adam.nilsson3@elev.ga.ntig.se>
2024-05-08 20:32:12 +0200

    2024-05-08

--- app.rb
+++ app.rb
@@ -7,34 +7,55 @@ require './model.rb'
 
 enable:sessions
 
+include Model
+
+MAX_LOGIN_ATTEMPTS = 3
+COOLDOWN_PERIOD = 60
+
+#check if user is admin
 helpers do
     def admin
         return session[:role] == 1
     end
 end
 
+#checks if user is logged in
 before('/p/*') do
     p "These are protected_methods"
      if session[:id] ==  nil
       redirect('/')
     end
 end
-   
-post('/login')do
 
+#attemts to log in user
+#
+# @param [string]:username, username of user
+# @param [string]:password, password of user
+#
+# @see Model#get_user_from_username
+# @see Model#check_password
+post('/login') do
     username = params[:username]
     password = params[:password]
 
-    if get_user(username).empty?
-        flash[:notice] = "no user with that username"
+    failed_login_attempts = session["#{username}_failed_login_attempts"] || 0
+    last_failed_login_time = session["#{username}_last_failed_login_time"] || Time.now - COOLDOWN_PERIOD - 1
+
+    if failed_login_attempts >= MAX_LOGIN_ATTEMPTS && Time.now - last_failed_login_time < COOLDOWN_PERIOD
+        flash[:notice] = "You have exceeded the maximum number of login attempts. Please wait #{COOLDOWN_PERIOD} seconds before trying again."
+        redirect('/')
+    elsif get_user_from_username(username).empty?
+        flash[:notice] = "No user with that username."
         redirect('/')
     else
-        result = get_user(username).first
+        result = get_user_from_username(username).first
         pwdigest = result["pwdigest"]
         id = result["id"]
         role = result["role"]
 
-        if check_password(pwdigest,password)
+        if check_password(pwdigest, password)
+            session["#{username}_failed_login_attempts"] = 0
+
             session[:id] = id
             session[:role] = role
             if role == 1
@@ -43,12 +64,28 @@ post('/login')do
                 redirect('/p/home')
             end
         else
-            flash[:notice] = "wrong password"
+        
+            session["#{username}_failed_login_attempts"] = failed_login_attempts + 1
+            session["#{username}_last_failed_login_time"] = Time.now
+
+            if failed_login_attempts + 1 >= MAX_LOGIN_ATTEMPTS
+                flash[:notice] = "You have exceeded the maximum number of login attempts. Please wait #{COOLDOWN_PERIOD} seconds before trying again."
+            else
+                flash[:notice] = "Wrong password. You have #{MAX_LOGIN_ATTEMPTS - (failed_login_attempts + 1)} attempts remaining."
+            end
             redirect('/')
         end
     end
 end
 
+# Attemts to register a user
+#
+# @param [string]:username, username of user
+# @param [string]:password, password of user
+# @param [string]:password_confirm, password_confirm of user
+#
+# @see Model#get_usernames
+# @see Model#register_user
 post('/register')do
 
     username = params[:username]
@@ -56,26 +93,32 @@ post('/register')do
     password_confirm = params[:password_confirm]
     role = 0
 
-    if get_usernames.include?([username])
+    if username == "" || password == "" || password_confirm == ""
+        flash[:notice] = "rutorna måste vara ifyllda"
+    elsif get_usernames.include?([username])
         flash[:notice] = "already existing username"
-        redirect('/showregister')
     elsif (password == password_confirm)
         register_user(username,password,role)
         redirect('/')
     else
         flash[:notice] = "passwords did not match"
-        redirect('/showregister')
     end
+    redirect('/showregister')
 end
 
+#Display register page
 get('/showregister')do
     slim(:register)
 end
 
+#Display login page
 get('/')do 
     slim(:login)
 end
 
+#display home page when loged in
+#
+#@see Model#show:home
 get('/p/home')do
     user_id = session[:id]
 
@@ -88,6 +131,9 @@ get('/p/home')do
     slim(:"/home",locals:{brands_result:brand,items_result:item,user_result:user})
 end
 
+#dispay user shoppin cart
+#
+#@see Model#show_user_shoppingcart
 get('/p/shopping_cart')do
 
     id = session[:id].to_i
@@ -99,6 +145,11 @@ get('/p/shopping_cart')do
 
 end
 
+#Attempts adding item to shopping cart
+#
+# @param [integer]:id, the id of item
+#
+# @see Model#add_to_shoppingcart
 post('/p/shopping_cart/add')do
 
     item_id = params[:item_id].to_i
@@ -110,19 +161,31 @@ post('/p/shopping_cart/add')do
 
 end
 
+#Attempts deleting item from shopping car
+#
+# @param [integer]:relation_id, id of the relation
+# 
+# @see Model#get_user_id_from_relation_id
+# @see Model#delete_from_shoppingcart
 post('/p/shopping_cart/delete')do
 
     relation_id = params[:relation_id].to_i
     user_id = session[:id].to_i
+    result = get_user_id_from_relation_id(relation_id)
+    connected_user_id = result["user_id"]
 
-    p relation_id
-  
-    delete_from_shoppingcart(relation_id)
-
+    if connected_user_id == user_id
+        delete_from_shoppingcart(relation_id)
+    else 
+        redirect("/p/error")
+    end
     redirect('/p/shopping_cart')
 
 end
 
+#displays all items
+#
+#see Model#show_items_admin
 get('/p/items_admin') do
 
     unless admin
@@ -135,6 +198,11 @@ get('/p/items_admin') do
 
 end
 
+# Attemts to delete item
+#
+# @param [Interger]:item_id, id of item
+#
+# see Model#delete_from_items_admin
 post('/p/items_admin/delete')do
 
     unless admin
@@ -150,6 +218,14 @@ post('/p/items_admin/delete')do
 
 end
 
+# Attemts to create new item
+#
+# @param [interger]:new_model_id, id of model of item
+# @param [interger]:new_brand_id, id of brad of item
+# @param [string]:new_name, name of itemm
+# @param [string]:new_price, price of item¨
+#
+# @see Model#create_item
 post('/p/items_admin/new')do
 
     unless admin
@@ -162,17 +238,23 @@ post('/p/items_admin/new')do
     new_name = params[:new_name]
     new_price = params[:new_price]
 
-    create_item(new_model_id,new_brand_id,new_name,new_price)
+    if new_name == "" ||  new_price == ""
+        flash[:notice] = "rutorna måste vara ifyllda"
+    else
+        create_item(new_model_id,new_brand_id,new_name,new_price)
+    end
 
     redirect('/p/items_admin')
-
-
 end
 
+# Display error site
 get("/p/error")do
    slim(:cheater)
 end
 
+# display list of users
+#
+# @see Model#get_all_user_info
 get('/p/user_list_admin')do
 
     unless admin
@@ -185,6 +267,12 @@ get('/p/user_list_admin')do
 
 end
 
+# Attemots to delete user
+#
+# @param [interger]:user_id, id of user
+#
+# @see Model#delete_all_user_items
+# @see Model#delete_user
 post('/p/user_list_admin/delete')do
 
     unless admin
@@ -200,6 +288,14 @@ post('/p/user_list_admin/delete')do
 
 end
 
+# Attemots to add user
+#
+# @param [string]:username, username of user
+# @param [string]:password, password of user
+# @param [string]:password_confrim, password_confirm of user
+# @param [interger]:role, role of user
+#
+# @see Model#register_user
 post('/p/user_list_admin/add')do
 
     unless admin
@@ -211,7 +307,10 @@ post('/p/user_list_admin/add')do
     password_confirm = params[:password_confirm]
     role = params[:role].to_i
 
-    if get_usernames.include?([username])
+    if username == "" || password == "" || password_confirm == ""
+        flash[:notice] = "rutorna måste vara ifyllda"
+
+    elsif get_usernames.include?([username])
         flash[:notice] = "already existing username"
         redirect('/p/user_list_admin')
     elsif (password == password_confirm)
@@ -221,18 +320,72 @@ post('/p/user_list_admin/add')do
         flash[:notice] = "passwords did not match"
         redirect('/p/user_list_admin')
     end
-
     redirect('/p/user_list_admin')
 
 end
 
+# Displays ability to change user info
 get("/p/edit_user")do
-
-
-    slim()
+    slim(:edit_user)
 end
 
-post("logout")do
+# Attempts to change username
+#
+# @param [string]:new_username, new username of user
+# @param [string]:new_username_confirm, username confrim of new user
+#
+# @see Model#update_user_username
+# @see model#get_usernames
+post("/p/edit_user_username")do
+
+    username = params[:new_username]
+    username_confirm = params[:new_username_confirm]
+    user_id = session[:id]
+    
+    if username == "" || username_confirm ==""
+        flash[:notice] = "rutorna måste vara ifyllda"
+    elsif get_usernames.include?([username])
+        flash[:notice] = "upptaget användarnamn"
+    elsif (username == username_confirm)
+        update_user_username(user_id,username)
+        flash[:notice] = "användarnam uppdaterat till #{username}"
+    else
+        flash[:notice] = "användarnamnen matchade inte"
+    end
+    redirect("/p/edit_user")
+end
+
+# Attempts to change password
+#
+# @param [string]:old_password, old password of user
+# @param [string]:new_password, new_password user
+# @param [string]:new_password_confirm, new_password_confirm for user
+#
+# @see Model#get_password_from_id
+# @see model#check_password
+# @see model#update_user_password
+post("/p/edit_user_password")do
+
+    result = get_password_from_id(session[:id])
+    old_pwdigest = result["pwdigest"]
+    old_password = params[:old_password]
+    new_password = params[:new_password]
+    new_password_confirm = params[:new_password_confirm]
+
+    if old_password == ""|| new_password == ""||new_password_confirm == ""
+        flash[:notice] = "rutorna måste vara ifyllda"
+    elsif check_password(old_pwdigest,old_password)
+        if new_password == new_password_confirm
+            update_user_password(session[:id],new_password)
+            flash[:notice] = "Lösenordet är nu bytt till #{new_password}"
+        else
+            flash[:notice] = "ditt nya lösenord matchade inte"
+        end
+    else
+        flash[:notice] = "ditt gamla lösenord var fel"
+    end
+
+    redirect("/p/edit_user")
 end
 
 
--- model.rb
+++ model.rb
@@ -1,107 +1,172 @@
-def get_database()
-    db = SQLite3::Database.new('db/webshop.db')
-    db.results_as_hash = true
-    return db
-end
+module Model
 
-def get_user(username)
-    db = get_database()
-    result = db.execute("SELECT pwdigest, id, role FROM user WHERE username = ?",username)
-    return result
-end
+    # Finds database
+    #
+    # @return [hash] The database
+    def get_database()
+        db = SQLite3::Database.new('db/webshop.db')
+        db.results_as_hash = true
+        return db
+    end
 
-def check_password(pwdigest,password)
-    db = get_database()
-    return  BCrypt::Password.new(pwdigest) == password
-end
+    # Finds user information from username
+    #
+    # @return [hash] User info
+    def get_user_from_username(username)
+        db = get_database()
+        result = db.execute("SELECT pwdigest, id, role FROM user WHERE username = ?",username)
+        return result
+    end
 
-def get_all_user_info()
+    # Chekcs passwords
+    #
+    # @return [boolean] If passwords match
+    def check_password(pwdigest,password)
+        db = get_database()
+        return  BCrypt::Password.new(pwdigest) == password
+    end
 
-    db = get_database()
+    # Finds users
+    #
+    # @return [hash] Users info
+    def get_all_user_info()
+        db = get_database()
+    
+        result = db.execute("SELECT pwdigest, id, role, username FROM user")
+
+        return result
+
+    end
+
+    # Find usernames
+    #
+    # @return [array] Array with usernames
+    def get_usernames()
+        db = SQLite3::Database.new('db/webshop.db')
+        result = db.execute("SELECT username FROM user")
+        return result
+    end
+
+    # Attempts to register user
+    def register_user(username,password,role)
+        db = get_database()
+        password_digest = BCrypt::Password.create(password)
+        db.execute("INSERT INTO user (username,pwdigest,role) VALUES (?,?,?)",username,password_digest,role)
+    end
+
+    # Finds Users selected items
+    #
+    # @return [hash] chosen items
+    def show_user_shoppingcart(id)
+        db = get_database()
+        items = db.execute("SELECT user_items_rel.id, items.name, items.price, model.name AS model_name, brand.name AS brand_name, items_id
+        FROM (((user_items_rel 
+        INNER JOIN items ON user_items_rel.items_id = items.id)
+        INNER JOIN model ON items.model_id = model.id)
+        INNER JOIN brand ON items.brand_id = brand.id)
+        WHERE user_id = #{id}
+        ")
+        return items
+    end 
+
+    # Attempts to add item to shoppingcart
+    def add_to_shoppingcart(item_id,user_id)
+        db = get_database()
+        db.execute("INSERT INTO user_items_rel (user_id, items_id) VALUES (?,?)",user_id,item_id)
+    end
+
+    # Attempts to delete item from shoppingcart
+    def delete_from_shoppingcart(relation_id)
+        db = get_database
+        db.execute("DELETE FROM user_items_rel WHERE id =?",relation_id)
+    end
+
+    # Finds all items
+    #
+    # return [hash] with all items
+    def show_items_admin()
+        db = get_database()
+        result = db.execute ("SELECT items.*, brand.name AS brand_name, model.name AS model_name
+        FROM items
+        INNER JOIN brand ON items.brand_id = brand.id
+        INNER JOIN model ON items.model_id = model.id")
+    end
  
-    result = db.execute("SELECT pwdigest, id, role, username FROM user")
+    # Attemptst to create item
+    def create_item(new_model_id,new_brand_id,new_name,new_price)
+        db = get_database()
+        db.execute("INSERT INTO items (name, price, model_id, brand_id ) VALUES (?,?,?,?)",new_name,new_price,new_model_id,new_brand_id)
+    end
 
-    return result
+    # Attempts to delete item
+    def delete_from_items_admin(item_id)
 
-end
+        db = get_database()
+        db.execute("DELETE FROM items WHERE id = ?", item_id)
+        db.execute("DELETE FROM user_items_rel WHERE items_id = ?", item_id)
 
-def get_usernames()
-    db = SQLite3::Database.new('db/webshop.db')
-    result = db.execute("SELECT username FROM user")
-    return result
-end
+    end
 
-def register_user(username,password,role)
-    db = SQLite3::Database.new('db/webshop.db')
-    password_digest = BCrypt::Password.create(password)
-    db.execute("INSERT INTO user (username,pwdigest,role) VALUES (?,?,?)",username,password_digest,role)
-end
+    #Finds information to display on home page
+    #
+    # return [array] Array with user, brand and item
+    def show_home(user_id)
 
-def show_user_shoppingcart(id)
+        db = get_database()
+
+        brand = db.execute("SELECT * FROM brand")
+        item = db.execute("SELECT * FROM items")
+        user = db.execute("SELECT username FROM user Where id =?",user_id).first
+
+        arr = [brand,item,user]
+
+        return arr
+    end
+
+    #attempts to delete user
+    def delete_user(user_id)
+
+        db = get_database()
+
+        db.execute("DELETE FROM user WHERE id = ?", user_id)
+
+    end
+
+    #attempts to delte user items
+    def delete_all_user_items(user_id)
+        db = get_database()
+        db.execute("DELETE FROM user_items_rel WHERE user_id =? ",user_id)
+    end
+
+    #attempts to update user's username
+    def update_user_username(user_id,username)
+        db = get_database()
+        db.execute("UPdate user SET username =? WHERE id =? ",username,user_id)
+
+    end
+
+    #Finds user password
+    #
+    # return [hash] user password
+    def get_password_from_id(user_id)
+        db = get_database()
+        return db.execute("SELECT pwdigest FROM user WHERE id = ?",user_id).first
+    end
+
+    #attempts to update user's password
+    def update_user_password(user_id,password)
+        db = get_database()
+        password_digest = BCrypt::Password.create(password)
+        db.execute("UPdate user SET pwdigest =? WHERE id =? ",password_digest,user_id)
+
+    end
+
+    #Finds user id connected to relation
+    #
+    # return [hash] user id
+    def get_user_id_from_relation_id(relation_id)
     db = get_database()
-    items = db.execute("SELECT user_items_rel.id, items.name, items.price, model.name AS model_name, brand.name AS brand_name, items_id
-    FROM (((user_items_rel 
-    INNER JOIN items ON user_items_rel.items_id = items.id)
-    INNER JOIN model ON items.model_id = model.id)
-    INNER JOIN brand ON items.brand_id = brand.id)
-    WHERE user_id = #{id}
-    ")
-    return items
-end 
+    return db.execute("SELECT user_id FROM user_items_rel WHERE id = ?",relation_id).first
+    end
 
-def add_to_shoppingcart(item_id,user_id)
-    db = get_database()
-    db.execute("INSERT INTO user_items_rel (user_id, items_id) VALUES (?,?)",user_id,item_id)
-end
-
-def delete_from_shoppingcart(relation_id)
-    db = get_database
-    db.execute("DELETE FROM user_items_rel WHERE id =?",relation_id)
-end
-
-def show_items_admin()
-    db = get_database()
-    result = db.execute ("SELECT items.*, brand.name AS brand_name, model.name AS model_name
-    FROM items
-    INNER JOIN brand ON items.brand_id = brand.id
-    INNER JOIN model ON items.model_id = model.id")
-end
-
-def create_item(new_model_id,new_brand_id,new_name,new_price)
-    db = get_database()
-    db.execute("INSERT INTO items (name, price, model_id, brand_id ) VALUES (?,?,?,?)",new_name,new_price,new_model_id,new_brand_id)
-end
-
-def delete_from_items_admin(item_id)
-
-    db = get_database()
-    db.execute("DELETE FROM items WHERE id = ?", item_id)
-    db.execute("DELETE FROM user_items_rel WHERE items_id = ?", item_id)
-
-end
-
-def show_home(user_id)
-
-    db = get_database()
-
-    brand = db.execute("SELECT * FROM brand")
-    item = db.execute("SELECT * FROM items")
-    user = db.execute("SELECT username FROM user Where id =?",user_id).first
-
-    arr = [brand,item,user]
-
-    return arr
-end
-
-def delete_user(user_id)
-
-    db = get_database()
-
-    db.execute("DELETE FROM user WHERE id = ?", user_id)
-
-end
-
-def delete_all_user_items(user_id)
-    db = get_database()
-    db.execute("DELETE FROM user_items_rel WHERE user_id =? ",user_id)
 end
